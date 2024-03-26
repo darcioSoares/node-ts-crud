0044ba0469ff56e6febb151241ddc5d72f005ba1
darcioSoares <darciosoares28@gmail.com>
2024-03-25 21:47:29 -0300

    feat (db) : add um banco de dado local, salva em arquilo

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,26 +1,95 @@
-// user.controller.ts
+let NeDB = require('nedb')
+let db = new NeDB({
+    filename: 'src/db/users.db',
+    autoload: true
+})
 
-import { Request, Response } from 'express';
+
+import { Request, Response, application } from 'express';
 
 class UserController {
     public createUser(req: Request, res: Response): void {
-        // Lógica para criar um usuário
-        console.log('createUser')
+
+        db.insert(req.body,(err,user) => {
+           if(err){
+                console.log("error: ", err)
+                res.json({"error":err})
+           }else{
+                console.log('createUser')
+                res.json({"user":user})
+           }
+        })        
+
     }
 
     public getUser(req: Request, res: Response): void {
-        // Lógica para obter um usuário
-        console.log('getUser')
+
+        let params = req.params.id
+
+        db.findOne({_id:params}).exec((err, users)=>{
+            if(err){
+                console.log('error: ', err)
+                res.status(400).json({
+                    error:err
+                })
+            }else{
+                res.statusCode = 200;
+                res.setHeader("Content-type","application/json");
+                res.json({
+                    users: users
+                })
+            }
+        })
+    }
+
+    public getAllUser(req: Request, res: Response): void {
+
+        db.find({}).sort({name:1}).exec((err, users)=>{
+            if(err){
+                console.log('error: ', err)
+                res.status(400).json({
+                    error:err
+                })
+            }else{
+                res.statusCode = 200;
+                res.setHeader("Content-type","application/json");
+                res.json({
+                    users: users
+                })
+            }
+        })
     }
 
     public updateUser(req: Request, res: Response): void {
-        // Lógica para atualizar um usuário
-        console.log('updateUser')
+        let params = req.params.id
+        
+        db.update({_id:params}, req.body, err => {
+
+            if(err){
+                console.log('error: ', err)
+                res.status(400).json({
+                    error:err
+                })
+            }else{
+                res.statusCode = 200;
+                res.setHeader("Content-type","application/json");
+                res.json({
+                    users: Object.assign(req.params, req.body)
+                })
+            }
+        })
     }
 
     public deleteUser(req: Request, res: Response): void {
-        // Lógica para excluir um usuário
-        console.log('deleteUser')
+        db.remove({_id:req.params.id},{}, err=>{
+            if(err){
+                res.statusCode = 401
+                res.json(err)
+            }else{
+                res.statusCode = 201
+                res.json(req.params)
+            }
+        })
     }
 }
 
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -5,6 +5,7 @@ const router = Router();
 const userController = new UserController();
 
 router.post('/', userController.createUser);
+router.get('/', userController.getAllUser);
 router.get('/:id', userController.getUser);
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
